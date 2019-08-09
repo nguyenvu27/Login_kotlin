8a46f65f55142457ba191ebc8e1117ca788c59ba
Your Name <you@example.com>
2019-08-09 11:31:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/authentcation/activities/ProfileActivity.kt
@@ -0,0 +1,25 @@
+package com.example.authentcation.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.authentcation.R
+import simplifiedcoding.net.kotlinretrofittutorial.storage.SharedPrefManager
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//
+//        if(!SharedPrefManager.getInstance(this).isLoggedIn){
+//            val intent = Intent(applicationContext, LoginActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            startActivity(intent)
+//        }
+//    }
+}
---
+++ app/src/main/java/com/example/authentcation/models/LoginResponse.kt
@@ -0,0 +1,4 @@
+package simplifiedcoding.net.kotlinretrofittutorial.models
+
+
+data class LoginResponse(val error: Boolean, val message:String, val user: User)
